27f98d8b452ba0ed91cbef868f20f8af6bc69bb8
Loaderon <alvaroscelza@gmail.com>
2021-10-24 22:03:17 -0300

    added Flex theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,11 +2,8 @@ AUTHOR = 'Ing. Alvaro Rodríguez Scelza'
 SITENAME = 'Alvaro Scelza'
 SITEURL = ''
 
-PATH = 'content'
-
-TIMEZONE = 'America/Montevideo'
-
 DEFAULT_LANG = 'Spanish'
+DEFAULT_PAGINATION = False
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -20,12 +17,14 @@ LINKS = (('Pelican', 'https://getpelican.com/'),
          ('Python.org', 'https://www.python.org/'),
          ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
          ('You can modify those links in your config file', '#'),)
+PATH = 'content'
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
-DEFAULT_PAGINATION = False
+THEME = "themes/Flex"
+TIMEZONE = 'America/Montevideo'
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
