1ee84cd82d813720a63b8dd8c6bd9d076f5ea445
Alvaro Scelza <alvaroscelza@gmail.com>
2022-04-05 23:56:43 -0300

    fixed bug on social media links

--- src/components/Menu/SocialMediaLink.js
+++ src/components/Menu/SocialMediaLink.js
@@ -3,8 +3,8 @@ import classes from './SocialMediaLink.module.css';
 const SocialMediaLink = ({name, link, image}) => {
     return (
         <li className={classes.li}>
-            <a target="_blank" rel="noopener noreferrer" href={name}>
-                <img className={classes.img} src={image} alt={link} width='45' height='45'/>
+            <a target="_blank" rel="noopener noreferrer" href={link}>
+                <img className={classes.img} src={image} alt={name} width='45' height='45'/>
             </a>
         </li>
     );
