5a929d02a99b2c1a2d56ebd5b99285b8dac27678
Alvaro Scelza <alvaroscelza@gmail.com>
2024-01-03 14:06:01 -0300

    Updated home

--- src/components/Pages/HomePage.js
+++ src/components/Pages/HomePage.js
@@ -10,10 +10,12 @@ const HomePage = () => {
 
     const titleCertificationLink = "https://certificados.ort.edu.uy/app/vIndex.html?p1=181577-G8NPQF97";
     const designThinkingLink = "https://www.designthinking.es/inicio/index.php";
+    const continuousImprovementLink = "https://en.wikipedia.org/wiki/Continuous_improvement_process";
     const skollarsLink = "https://skollars.com/";
     const agileManifestoLink = "https://agilemanifesto.org/";
     const leanStartupLink = "https://en.wikipedia.org/wiki/Lean_startup";
     const uruguayanFlagLink = "https://s.w.org/images/core/emoji/13.1.0/svg/1f1fa-1f1fe.svg";
+    const italianFlagLink = "https://s.w.org/images/core/emoji/13.1.0/svg/1f1ee-1f1f9.svg";
     const unitedKingdomFlagLink = "https://s.w.org/images/core/emoji/13.1.0/svg/1f1ec-1f1e7.svg";
     const lapsaungSouchongLink = "https://en.wikipedia.org/wiki/Lapsang_souchong";
 
@@ -34,20 +36,24 @@ const HomePage = () => {
                     requirements elicitation and analysis, design (Architecture, OOP and User Interfaces), Software
                     building, testing and maintenance, with <InternalLink link='/knowledge' text='multiple technologies'/>.
                     I can also help you in managing projects and leading development
-                    teams. <ExternalLink link={skollarsLink} text="Take a look at all I can do for you"/>.
+                    teams.
                 </p>
                 <p>
-                    ➿ An enthusiast of <ExternalLink link={agileManifestoLink} text="agile software development"/> and
-                    also of design methodologies and business development,
-                    like <ExternalLink link={leanStartupLink} text="Lean Startup"/> and&nbsp;
-                    <ExternalLink link={designThinkingLink} text="Design Thinking"/>.
+                    ➿ An enthusiast of <ExternalLink link={agileManifestoLink} text="agile software development"/>,
+                    design and business development methodologies,
+                    like <ExternalLink link={leanStartupLink} text="Lean Startup"/>,&nbsp;
+                    <ExternalLink link={designThinkingLink} text="Design Thinking"/> and&nbsp;
+                    <ExternalLink link={continuousImprovementLink} text="Continuous Improvement"/>.
                 </p>
                 <p>
-                    🧩 I've worked in <InternalLink link='/projects' text='several projects'/>, some of them as an
-                    employee and others as entrepreneur.
+                    🧩 More than a decade in the Software industry, I've worked in&nbsp;
+                    <InternalLink link='/projects' text='dozens of projects'/>, as both employee and entrepreneur.
                 </p>
                 <p>
                     <img className={classes.emojiIcon} src={uruguayanFlagLink} alt="🇺🇾"/> From Uruguay :)
+                </p>
+                <p>
+                    <img className={classes.emojiIcon} src={italianFlagLink} alt="it"/> With Italian passport too.
                 </p>
                 <p>
                     <img className={classes.emojiIcon} src={unitedKingdomFlagLink} alt="🇬🇧"/>
@@ -57,11 +63,10 @@ const HomePage = () => {
                     ✈️ I love Travelling.
                 </p>
                 <p>
-                    ☕ Coffee adict! But I also enjoy types of teas
-                    (specially <ExternalLink link={lapsaungSouchongLink} text="Lapsang Souchong"/>).
+                    ☕ Coffee fanatic!
                 </p>
                 <p>
-                    📈 Crypto and blockchain technologies lover.
+                    📈 Crypto, blockchain and AI technologies lover.
                 </p>
             </div>
         </>
